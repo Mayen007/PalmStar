c2b0774164ae67cbc607534ae7c0c952d5296c85
Mayen007 <alonerpunk@gmail.com>
2024-04-12 10:32:13 +0300

    update index.html
    Major changes; added column-reverse to the footer item and made the icons coloured with relevant links added. Moved the Book Now button to the navbar for easier accessibility.

--- static/js/script.js
+++ static/js/script.js
@@ -5,3 +5,9 @@
     document.querySelector('.offcanvas-collapse').classList.toggle('open')
   })
 })()
+
+$('#staticBackdrop').modal({
+  backdrop: 'static',
+  keyboard: false,
+  zIndex: 1050 // Adjust the z-index as needed
+});
