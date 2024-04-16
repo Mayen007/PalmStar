cf2bec2cbb54d3c7cdac5cf92f51366d706a8f31
Mayen007 <alonerpunk@gmail.com>
2024-04-16 11:27:28 +0300

    update

--- static/js/script.js
+++ static/js/script.js
@@ -11,21 +11,3 @@ $('#staticBackdrop').modal({
   keyboard: false,
   zIndex: 1050 // Adjust the z-index as needed
 });
-
-document.addEventListener("DOMContentLoaded", function () {
-  const sections = document.querySelectorAll('.animated');
-
-  function checkViewport() {
-    sections.forEach(section => {
-      const sectionPosition = section.getBoundingClientRect().top;
-      const screenHeight = window.innerHeight;
-
-      if (sectionPosition < screenHeight * 0.75) {
-        section.classList.add('fadeIn');
-      }
-    });
-  }
-
-  window.addEventListener('scroll', checkViewport);
-  checkViewport();
-});
